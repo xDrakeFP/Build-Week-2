5dc1c31332c0ea00e8d9b416478a35599e170605
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2025-07-17 10:18:15 +0200

    commit 6

---
+++ assets/script/album.js
@@ -0,0 +1,96 @@
+const parameters = new URLSearchParams(window.location.search);
+const albumId = parameters.get("id");
+
+const endSearch =
+  " https://striveschool-api.herokuapp.com/api/deezer/search?q=";
+const endAlbum = "https://striveschool-api.herokuapp.com/api/deezer/album";
+
+const endArtist = "https://striveschool-api.herokuapp.com/api/deezer/artist";
+const content = document.getElementById("lista");
+const albumImg = document.querySelector("#container-img img");
+const titoloAlbum = document.querySelectorAll(".titolo-album-d");
+const titoloAlbum2 = document.querySelector("#titolo-album");
+const artistaAlbum = document.querySelectorAll(".artista-album");
+const annoAlbum = document.querySelectorAll(".anno-album");
+const numeroTracce = document.querySelectorAll(".numero-tracce");
+const durataAlbum = document.querySelectorAll(".durata-album");
+
+console.log(albumId);
+fetch(endAlbum + "/" + albumId)
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error("errore");
+    }
+  })
+  .then((data) => {
+    console.log(data);
+    const tracksArray = data.tracks.data;
+    console.log(tracksArray);
+    console.log(content);
+    albumImg.setAttribute("src", data.cover_big);
+    titoloAlbum[0].innerText = data.title;
+    titoloAlbum2.innerText = data.title;
+    artistaAlbum[0].innerText = data.artist.name;
+    artistaAlbum[1].innerText = data.artist.name;
+    annoAlbum[0].innerText = data.release_date.slice(0, -6);
+    annoAlbum[1].innerText = data.release_date.slice(0, -6);
+    numeroTracce[0].innerText = tracksArray.length.toString() + " brani";
+    let albumDuration = 0;
+    for (let i = 0; i < tracksArray.length; i++) {
+      albumDuration = albumDuration + tracksArray[i].duration;
+    }
+
+    let albumMinutes = Math.floor(albumDuration / 60);
+    let albumSeconds = Math.floor(albumDuration % 60);
+
+    durataAlbum[0].innerText =
+      albumMinutes.toString() + " min" + " " + albumSeconds.toString() + " sec";
+
+    for (let i = 0; i < tracksArray.length; i++) {
+      minutes = Math.floor(tracksArray[i].duration / 60);
+      seconds = Math.floor(tracksArray[i].duration % 60);
+
+      if (minutes < 10) {
+        minutes = "0" + minutes.toString();
+      } else {
+        minutes = minutes;
+      }
+
+      if (seconds < 10) {
+        seconds = "0" + seconds.toString();
+      } else {
+        seconds = seconds;
+      }
+
+      content.innerHTML += `
+        <div class="row">
+                      <div class="col d-none d-lg-block col-lg-1">
+                        <p class="text-white text-center">${i + 1}</p>
+                      </div>
+                      <div class="col col-11 col-lg-5">
+                        <a href='javascript:void(0)' class='text-decoration-none'><p class="text-white mb-1">${
+                          tracksArray[i].title_short
+                        }</p></a>
+                       <a href='./artist.html?id=${tracksArray[
+                         i
+                       ].artist.id.toString()}' class='text-decoration-none'> <p class="text-secondary">${
+        tracksArray[i].artist.name
+      }</p></a>
+                      </div>
+                      <div class="col d-none d-lg-block col-lg-3">
+                        <p class="text-white text-center">${Math.ceil(
+                          Math.random() * 1000000000
+                        )}</p>
+                      </div>
+                      <div class="col d-none d-lg-block col-lg-3">
+                        <p class="text-white text-center">${minutes}: ${seconds}</p>
+                      </div>
+                    </div>
+        `;
+    }
+  })
+  .catch((er) => {
+    console.log(er);
+  });
--- assets/script/artist.js
+++ assets/script/artist.js
@@ -50,15 +50,33 @@ const getTracks = function () {
   <div class="d-flex align-items-center my-4">
   <span style="width:1em" class="fs-4">${i + 1}</span>
     <img src="${imageUrl}" class="ms-4" height="100" width="100" alt="Album" />
-    <div class="d-flex flex-column flex-grow-1 ms-4 flex-lg-row">
+    <div class="d-flex flex-column flex-grow-1 ms-4 flex-xxl-row">
       <a href='javascript:void(0)' class='click text-decoration-none text-white'
         onclick="player('${title}', '${artist}', '${imageUrl}')">
-        <h5 style="width: 15em" class='me-4 text-white'>${title}</h5>
+        <h5 style="width: 8em" class='me-4 text-white'>${title}</h5>
       </a>
-      <p style="width: 4em" class="flex-lg-grow-1 text-center">Views</p>
-      <p class="d-none d-lg-block">${minutes}:${seconds}</p>
+      <p style="width: 4em" class="flex-lg-grow-1 text-center  ">${Math.ceil(
+        Math.random() * 5000000000
+      )}</p>
+      <p class="d-none d-xxl-block">${minutes}:${seconds}</p>
     </div>
-    
+    <button
+                type="button"
+                class="btn btn-dark text-secondary px-3 py-2 order-lg-3 d-lg-none"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-three-dots-vertical"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"
+                  />
+                </svg></button
+              >
   </div>
   
 `;
@@ -114,7 +132,7 @@ showBarMob.addEventListener("click", (e) => {
   searchBar.classList.toggle("d-none");
 });
 
-searchForm.addEventListener("submit", (e) => {
+/* searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   Risultati.classList.remove("d-none");
   fetch(endSearch + searchInput.value)
@@ -152,7 +170,70 @@ searchForm.addEventListener("submit", (e) => {
                     />
                   </div>
                   <div class="col col-6">
-                   <a href='javascript:void(0)' class='text-decoration-none text-white'> <h2>${data.data[i].title_short}</h2></a>
+                   <a href='javascript:void(0)' class='text-decoration-none text-white'
+                   
+                   > <h2>${data.data[i].title_short}</h2></a>
+                  </div>
+                  <div class="col col-2">
+                    <h5>${minutes}: ${seconds}</h5>
+                  </div>
+                </div>
+      `;
+      }
+      e.target.reset();
+    })
+    .catch((er) => {
+      console.log(er);
+    });
+}); */
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  Risultati.classList.remove("d-none");
+  fetch(endSearch + searchInput.value)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error("errore");
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      risultatiRicerca.innerHTML = "";
+      for (let i = 0; i < data.data.length; i++) {
+        let minutes = Math.floor(data.data[i].duration / 60);
+        let seconds = Math.floor(data.data[i].duration % 60);
+        if (minutes < 10) {
+          minutes = "0" + minutes.toString();
+        } else {
+          minutes = minutes;
+        }
+        if (seconds < 10) {
+          seconds = "0" + seconds.toString();
+        } else {
+          seconds = seconds;
+        }
+
+        let title = data.data[i].title_short.replace(/'/g, "\\'");
+        let artist = data.data[i].artist.name.replace(/'/g, "\\'");
+        let imageUrl = data.data[i].album.cover_big;
+
+        risultatiRicerca.innerHTML += `
+       <div class="row align-items-center my-3">
+                  <div class="col col-3">
+                    <a href='./album.html?id=${data.data[
+                      i
+                    ].album.id.toString()}'><img
+                      alt="cover"
+                      src=${imageUrl}
+                      class="w-100"
+                    /></a>
+                  </div>
+                  <div class="col col-6">
+                   <a href='javascript:void(0)' class='text-decoration-none text-white'
+                   onclick="player('${title}', '${artist}', '${imageUrl}')"
+                   > <h2>${title}</h2></a>
                   </div>
                   <div class="col col-2">
                     <h5>${minutes}: ${seconds}</h5>
@@ -170,12 +251,12 @@ searchForm.addEventListener("submit", (e) => {
 const closeSearch = function () {
   Risultati.classList.add("d-none");
   risultatiRicerca.innerHTML = "";
-  //Funzione per caricare sulla barra player un altra traccia
-  const addTitile = document.querySelector("click");
+};
+//Funzione per caricare sulla barra player un altra traccia
+const addTitile = document.querySelector("click");
 
-  const player = function (title, artist, imageUrl) {
-    document.getElementById("playerImg").src = imageUrl;
-    document.getElementById("playerTitle").innerText = title;
-    document.getElementById("playerArtist").innerText = artist;
-  };
+const player = function (title, artist, imageUrl) {
+  document.getElementById("playerImg").src = imageUrl;
+  document.getElementById("playerTitle").innerText = title;
+  document.getElementById("playerArtist").innerText = artist;
 };
