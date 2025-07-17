7f78b3194e549147352279c33308f212182b5691
KevR98 <ramil.kevin98@gmail.com>
2025-07-17 13:17:24 +0200

    adds, button

--- assets/script/artist.js
+++ assets/script/artist.js
@@ -27,7 +27,9 @@ const getTracks = function () {
     })
     .then((data1) => {
       console.log(data1);
-      for (let i = 0; i < data1.data.length; i++) {
+      content.innerHTML = '';
+      show.classList.remove('d-none');
+      for (let i = 0; i < 5; i++) {
         let duration = data1.data[i].duration;
         let minutes = Math.floor(duration / 60);
         if (minutes < 10) {
@@ -79,10 +81,10 @@ const getTracks = function () {
                 </svg></button
               >
   </div>
-  
 `;
       }
     })
+
     .catch((er) => {
       console.log(er);
     });
@@ -265,3 +267,82 @@ const player = function (title, artist, imageUrl) {
 const goBack = function () {
   window.history.back();
 };
+
+const showMore = function () {
+  fetch(tracklist)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error('errore');
+      }
+    })
+    .then((data1) => {
+      console.log(data1);
+      for (let i = 5; i < data1.data.length; i++) {
+        let duration = data1.data[i].duration;
+        let minutes = Math.floor(duration / 60);
+        if (minutes < 10) {
+          minutes = '0' + minutes.toString();
+        } else {
+          minutes = minutes;
+        }
+
+        let seconds = Math.floor(duration % 60);
+        if (seconds < 10) {
+          seconds = '0' + seconds.toString();
+        } else {
+          seconds = seconds;
+        }
+        //funzione che genera html dinamico
+        let title = data1.data[i].title.replace(/'/g, "\\'");
+        let artist = data1.data[i].artist.name.replace(/'/g, "\\'");
+        let imageUrl = data1.data[i].album.cover_big;
+
+        content.innerHTML += `
+  <div class="d-flex align-items-center my-4">
+  <span style="width:1em" class="fs-4">${i + 1}</span>
+    <img src="${imageUrl}" class="ms-4" height="100" width="100" alt="Album" />
+    <div class="d-flex flex-column flex-grow-1 ms-4 flex-xxl-row">
+      <a href='javascript:void(0)' class='click text-decoration-none text-white'
+        onclick="player('${title}', '${artist}', '${imageUrl}')">
+        <h5 style="width: 8em" class='me-4 text-white'>${title}</h5>
+      </a>
+      <p style="width: 4em" class="flex-lg-grow-1 text-center  ">${Math.ceil(
+        Math.random() * 5000000000
+      )}</p>
+      <p class="d-none d-xxl-block">${minutes}:${seconds}</p>
+    </div>
+    <button
+                type="button"
+                class="btn btn-dark text-secondary px-3 py-2 order-lg-3 d-lg-none"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-three-dots-vertical"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"
+                  />
+                </svg></button
+              >
+  </div>
+`;
+      }
+      const show = document.getElementById('show');
+      console.log(show);
+      show.classList.toggle('d-none');
+    })
+
+    .catch((er) => {
+      console.log(er);
+    });
+};
+
+const hide = document.getElementById('hide');
+console.log(hide);
+hide.classList.toggle('d-none');
